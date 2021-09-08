91cf89bb095776a7d10332210cb21cb8fdaac91f
Drew Schmidtke <amschmidtke@gmail.com>
2021-09-08 14:48:44 -0400

    initial form submission

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,13 +1,33 @@
+var userFormEL = document.querySelector("#user-form");
+var nameInputEl = document.querySelector("#username");
+
+var formSubmitHandler = function(event) {
+    event.preventDefault();
+    // get value from input element
+    var usernamer = nameInputEl.value.trim();
+
+    if (username) {
+        getUserRepos(username);
+        nameInputEl.value = "";
+    } else {
+        alert("Please enter a Github username");
+    }
+};
+
 var getUserRepos = function(user) {
-    // for mat the github api url
-    var apiUrl = "https://api.github.com/users/" + user + "/repos";
+    // format the github api url
+    var apiUrl = 'https://api.github.com/users/' + user + '/repos';
 
     // make a request to the url
     fetch(apiUrl).then(function(response) {
-        response.json().then(function(data) {
-            console.log(data);
+        if (response.ok) {
+            console.log(response);
+            response.json().then(function(data) {
+              console.log(data);
         });
+    };
     });
 };
 
-getUserRepos();
+
+userFormEL.addEventListener("submit", formSubmitHandler);
