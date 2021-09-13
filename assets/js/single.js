e4770bbf7d34753050b98617abff068da07ef4c2
Drew Schmidtke <amschmidtke@gmail.com>
2021-09-13 16:52:44 -0400

    search by language

--- assets/js/single.js
+++ assets/js/single.js
@@ -3,17 +3,18 @@ var issueContainerEl = document.querySelector("#issues-container");
 var limitWarningEl = document.querySelector("#limit-warning");
 
 var getRepoName = function() {
-  // grab repo bname from url query string
+  // grab repo name from url query string
   var queryString = document.location.search;
   var repoName = queryString.split("=")[1];
 
-  if(repoName) {
+  if (repoName) {
     // display repo name on the page
-  repoNameEl.textContent = repoName;
-  getRepoIssues(repoName);
+    repoNameEl.textContent = repoName;
+
+    getRepoIssues(repoName);
   } else {
     // if no repo was given, redirect to the homepage
-    document.location.replace('./index/html');
+    document.location.replace("./index.html");
   }
 };
 
@@ -33,8 +34,7 @@ var getRepoIssues = function(repo) {
           displayWarning(repo);
         }
       });
-    }
-    else {
+    } else {
       // if not successful, redirect to homepage
       document.location.replace("./index.html");
     }
@@ -54,12 +54,11 @@ var displayIssues = function(issues) {
     issueEl.classList = "list-item flex-row justify-space-between align-center";
     issueEl.setAttribute("href", issues[i].html_url);
     issueEl.setAttribute("target", "_blank");
-    
+
     // create span to hold issue title
     var titleEl = document.createElement("span");
     titleEl.textContent = issues[i].title;
-    
-  
+
     // append to container
     issueEl.appendChild(titleEl);
 
@@ -69,14 +68,13 @@ var displayIssues = function(issues) {
     // check if issue is an actual issue or a pull request
     if (issues[i].pull_request) {
       typeEl.textContent = "(Pull request)";
-    }
-    else {
+    } else {
       typeEl.textContent = "(Issue)";
     }
 
     // append to container
     issueEl.appendChild(typeEl);
-  
+
     // append to the dom
     issueContainerEl.appendChild(issueEl);
   }
